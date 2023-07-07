85e77aeb1160cb3548f8ec39b2758c835ca31dc8
Chris White <chris@inspiredsoftware.com.au>
2023-07-07 12:06:38 +1000

    IMP: Allow specifying an endpoint base_url for API requests

--- myob/api.py
+++ myob/api.py
@@ -1,11 +1,13 @@
 from .credentials import PartnerCredentials
 from .endpoints import ALL, ENDPOINTS, GET
 from .managers import Manager
-
+from .constants import MYOB_BASE_URL
 
 class Myob:
     """ An ORM-like interface to the MYOB API. """
-    def __init__(self, credentials):
+    def __init__(self, credentials, base_url=None):
+        if not base_url:
+            base_url = MYOB_BASE_URL
         if not isinstance(credentials, PartnerCredentials):
             raise TypeError(
                 'Expected a Credentials instance, got %s.' % (
@@ -13,8 +15,8 @@ class Myob:
                 )
             )
         self.credentials = credentials
-        self.companyfiles = CompanyFiles(credentials)
-        self._manager = Manager('', credentials, raw_endpoints=[
+        self.companyfiles = CompanyFiles(credentials, base_url)
+        self._manager = Manager('', credentials, base_url, raw_endpoints=[
             (GET, 'Info/', 'Return API build information for each individual endpoint.'),
         ])
 
@@ -26,9 +28,10 @@ class Myob:
 
 
 class CompanyFiles:
-    def __init__(self, credentials):
+    def __init__(self, credentials, base_url):
+        self.base_url = base_url
         self.credentials = credentials
-        self._manager = Manager('', self.credentials, raw_endpoints=[
+        self._manager = Manager('', self.credentials, base_url, raw_endpoints=[
             (ALL, '', 'Return a list of company files.'),
             (GET, '[id]/', 'List endpoints available for a company file.'),
         ])
@@ -36,7 +39,7 @@ class CompanyFiles:
 
     def all(self):
         raw_companyfiles = self._manager.all()
-        return [CompanyFile(raw_companyfile, self.credentials) for raw_companyfile in raw_companyfiles]
+        return [CompanyFile(raw_companyfile, self.credentials, self.base_url) for raw_companyfile in raw_companyfiles]
 
     def get(self, id, call=True):
         if call:
@@ -45,24 +48,25 @@ class CompanyFiles:
             # on the GET endpoint. The only way we currently allow passing company_id is by setting it on the manager,
             # and we can't do that on init, as this is a manager for company files plural..
             # Reluctant to change manager code, as it would add confusion if the inner method let you override the company_id.
-            manager = Manager('', self.credentials, raw_endpoints=[(GET, '', '')], company_id=id)
+            manager = Manager('', self.credentials, self.base_url, raw_endpoints=[(GET, '', '')], company_id=id)
             raw_companyfile = manager.get()['CompanyFile']
         else:
             raw_companyfile = {'Id': id}
-        return CompanyFile(raw_companyfile, self.credentials)
+        return CompanyFile(raw_companyfile, self.credentials, self.base_url)
 
     def __repr__(self):
         return self._manager.__repr__()
 
 
 class CompanyFile:
-    def __init__(self, raw, credentials):
+    def __init__(self, raw, credentials, base_url):
         self.id = raw['Id']
         self.name = raw.get('Name')
         self.data = raw  # Dump remaining raw data here.
         self.credentials = credentials
+        self.base_url = base_url
         for k, v in ENDPOINTS.items():
-            setattr(self, v['name'], Manager(k, credentials, endpoints=v['methods'], company_id=self.id))
+            setattr(self, v['name'], Manager(k, credentials, base_url, endpoints=v['methods'], company_id=self.id))
 
     def __repr__(self):
         return 'CompanyFile:\n    %s' % '\n    '.join(sorted(v['name'] for v in ENDPOINTS.values()))
